ce97e66e18b4d730539ab844b103c0834f261adf
Zach Smith <zsmith126028@gmail.com>
2020-03-03 12:44:19 -0600

    find home games for Rutgers. Probably need to make this a function and pas in necessary teams as arguments

--- big10.py
+++ big10.py
@@ -5,3 +5,17 @@ database = pymongo.MongoClient("mongodb://localhost:27017")
 bballDB = database["collegeBasketball"]
 
 big10 = bballDB["big10"].find()
+
+homePoints = 0
+homeGames = []
+awayPoints = 0
+awayGames = []
+
+for item in big10:
+  if item["Home"] == "Rutgers":
+    homeGames.append(item)
+    if item["HOMEPTS"] != '':
+      homePoints += item["HOMEPTS"]
+
+print(homePoints)
+print(homeGames)
