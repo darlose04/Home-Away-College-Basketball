5edf216a71c46282d827924b5177704fafb372fe
Zach Smith <zsmith126028@gmail.com>
2020-03-03 12:08:43 -0600

    add for loop to add up total points for home and away games

--- basketball.py
+++ basketball.py
@@ -6,9 +6,23 @@ bballDB = database["collegeBasketball"]
 
 x = bballDB["acc"].find()
 
+acc = bballDB["acc"].find()
+
 awayPoints = 0
+homePoints = 0
 
-for item in bballDB["acc"].find():
-  awayPoints += item["AWAYPTS"]
+for item in acc:
+  if item["AWAYPTS"] != '':
+    awayPoints += item["AWAYPTS"]
+  if item["HOMEPTS"] != '':
+    homePoints += item["HOMEPTS"]
 
-print(awayPoints)
+
+
+# entry = bballDB["acc"].find_one()
+# print(entry["AWAYPTS"])
+# print(type(entry["AWAYPTS"]))
+
+
+print("Visitor total points: ", awayPoints)
+print("Home total points: ", homePoints)
