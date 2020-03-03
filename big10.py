4ab39c790940b06f596f457123154d85e9d92d17
Zach Smith <zsmith126028@gmail.com>
2020-03-03 13:04:28 -0600

    create function to pass team names into

--- big10.py
+++ big10.py
@@ -9,16 +9,28 @@ bballDB = database["collegeBasketball"]
 
 big10 = bballDB["big10"].find()
 
-homePoints = 0
-homeGames = []
-awayPoints = 0
-awayGames = []
+def games(visitor, home):
+  homePoints = 0
+  homeGames = []
+  awayPoints = 0
+  awayGames = []
 
-for item in big10:
-  if item["Home"] == "Rutgers":
-    homeGames.append(item)
-    if item["HOMEPTS"] != '':
-      homePoints += item["HOMEPTS"]
+  for item in big10:
+    if item["Home"] == home:
+      homeGames.append(item)
+      if item["HOMEPTS"] != '':
+        homePoints += item["HOMEPTS"]
 
-print(homePoints)
-pp.pprint(homeGames)
+  for item in big10:
+    if item["Visitor"] == visitor:
+      awayGames.append(item)
+      if item["AWAYPTS"] != '':
+        awayPoints += item["AWAYPTS"]
+
+  print(homePoints / 9)
+  pp.pprint(homeGames)
+
+  print(awayPoints)
+  pp.pprint(awayGames)
+
+# print(games("Maryland", "Rutgers"))
